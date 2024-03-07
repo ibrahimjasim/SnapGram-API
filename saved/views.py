f9170c1a1fcec7ca2ec4c758b3223a3a1f3fd86c
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-07 12:41:45 +0000

    added SavedList generics view

--- saved/views.py
+++ saved/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from saved.models import Saved  
+from saved.serializers import SavedSerializer
 
-# Create your views here.
+
+class SavedList(generics.ListCreateAPIView):
+    """
+    List saved items or create a saved item if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = SavedSerializer 
+    queryset = Saved.objects.all()  
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
