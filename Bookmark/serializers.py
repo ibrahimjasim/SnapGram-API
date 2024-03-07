b43a5f1c3e030e0aa4eced3e5e8a5e1ba458b7ed
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-07 11:47:27 +0000

    Custom create method to handle the creation of Bookmark

--- Bookmark/serializers.py
+++ Bookmark/serializers.py
@@ -4,12 +4,25 @@ from .models import Bookmark
 
 class BookmarkSerializer(serializers.ModelSerializer):
     """
-    Serializer class for handling Bookmark data.
+    Serializer class for handling Bookmark data. Ensures that bookmarks are uniquely created
+    and handles potential duplicate bookmark attempts gracefully.
     """
-    # Define a read-only field for the owner's username to prevent it from being set directly
+
     owner_username = serializers.CharField(source='owner.username', read_only=True)
 
     class Meta:
         model = Bookmark  
-        fields = ['id', 'owner_username', 'post', 'created_at']  
-        read_only_fields = ('id', 'created_at')  
+        fields = ['id', 'owner_username', 'post', 'created_at'] 
+        read_only_fields = ('id', 'created_at') 
+
+    def create(self, validated_data):
+        """
+        Custom create method to handle the creation of Bookmark instances,
+        catching IntegrityError to handle possible duplicate entries.
+        """
+        try:
+            
+            return super().create(validated_data)
+        except IntegrityError as e:
+           
+            raise serializers.ValidationError({'detail': 'This bookmark already exists.'}) from e
