2259e66725de23146b314c00a45cdae4ae6c6afe
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-07 12:07:45 +0000

    Delete  bookmark app

--- Bookmark/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from bookmark import views
-
-urlpatterns = [
-    path('bookmark/', views.BookmarkList.as_view()),
-    path('bookmark/<int:pk>/', views.BookmarkDetail.as_view()),
-]
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('', include('bookmark.urls')),
-    
+       
 ]
