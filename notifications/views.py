c77b8b0572171e2a6a18be86e82313d8f34d62a8
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-07 21:54:19 +0000

    added NotificationList, NotificationDetail generics views

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from drf_api.permissions import IsNotificationOwner
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationList(generics.ListAPIView):
+    """List a user's notifications. No create view, since notifications are
+    automatically created.
+    """
+
+    serializer_class = NotificationSerializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["created_at", "is_read"]
+    
+
+    def get_queryset(self):
+        return Notification.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class NotificationDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Retrieve or delete a notification by id.
+    Restricted to notification owner.
+    """
+
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsNotificationOwner]
