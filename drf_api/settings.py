55598a53c9039d40aadf15e2eed50cac098f7c75
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-03-11 09:46:36 +0000

    added CLIENT_ORIGIN_DEV, and changed my heroku string to ALLOWED_HOST

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 
 if os.path.exists('env.py'):
@@ -60,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-ibrahimjasi-snapgramapi-5x9t7atcz5z.ws-eu108.gitpod.io', 'snapgram-api-df7c5b682dbd.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ibrahimjasi-snapgramapi-5x9t7atcz5z.ws-eu108.gitpod.io', 'ALLOWED_HOST']
 
 
 # Application definition
@@ -110,10 +111,11 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
-else:
-     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
