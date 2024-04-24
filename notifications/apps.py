6a9d7e710abab482d14ed33c0fda83b901c0f305
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-04-24 17:29:07 +0000

    indentation is correction

--- notifications/apps.py
+++ notifications/apps.py
@@ -5,6 +5,7 @@ class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
+    def ready(self):
+        import notifications.signals
 
-def ready(self):
-    import notifications.signals
+
