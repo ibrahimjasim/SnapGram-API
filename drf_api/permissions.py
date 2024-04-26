6e626844360cd4b0db81b901fadeffce1624fdc1
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-04-26 09:20:39 +0000

    Remove more files for the Notification app

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -7,15 +7,3 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.owner == request.user
-
-
-class IsNotificationOwner(permissions.BasePermission):
-    """A custom permission class that restricts access to notifications, 
-    ensuring that only the owner of the notification can view or modify it."""
-
-    def has_object_permission(self, request, view, obj):
-        if obj.owner == request.user:
-            return True
-        raise PermissionDenied(
-            "403 Permission denied"
-        )
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -31,6 +31,5 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('', include('saved.urls')),
-    path('', include('notifications.urls')),      
+    path('', include('saved.urls')),   
 ]
