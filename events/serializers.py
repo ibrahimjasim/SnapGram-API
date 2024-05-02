8241017ee86c0def7ac72144f115fe88bb22b3a3
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-05-02 11:01:13 +0000

    added my views.py and serializers to the events app

---
+++ events/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from django.utils import timezone
+from .models import Event
+
+class EventSerializer(serializers.ModelSerializer):
+    organizer_username = serializers.ReadOnlyField(source='organizer.username')  # Display organizer's username
+
+    class Meta:
+        model = Event
+        fields = ['id', 'title', 'description', 'location', 'start_time', 'end_time', 'organizer', 'organizer_username', 'created_at', 'updated_at']
+        read_only_fields = ['organizer', 'created_at', 'updated_at']  # These fields should not be editable by the API
+
+    def validate_start_time(self, value):
+        """
+        Check that the start_time is not in the past.
+        """
+        if value < timezone.now():
+            raise serializers.ValidationError("Start time cannot be in the past.")
+        return value
+
+    def validate(self, data):
+        """
+        Check that the end time is later than the start time.
+        """
+        if data['end_time'] <= data['start_time']:
+            raise serializers.ValidationError({"end_time": "End time must be after the start time."})
+        return data
+
+    def create(self, validated_data):
+        """
+        Create and return a new Event instance, given the validated data.
+        """
+        # The organizer is set in the view's perform_create method
+        return Event.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing Event instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.location = validated_data.get('location', instance.location)
+        instance.start_time = validated_data.get('start_time', instance.start_time)
+        instance.end_time = validated_data.get('end_time', instance.end_time)
+        instance.save()
+        return instance
--- events/views.py
+++ events/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Sets the organizer to the user who makes the request
+        serializer.save(organizer=self.request.user)
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned events to those with a status
+        indicating they are upcoming, if the 'upcoming' query parameter
+        is set in the URL.
+        """
+        queryset = self.queryset
+        if 'upcoming' in self.request.query_params:
+            queryset = queryset.filter(start_time__gt=timezone.now())
+        return queryset
