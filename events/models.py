55cb3b508082861d48dbb89fcb4ba65b4209cb2a
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-05-02 10:52:58 +0000

    added my events models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=300)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='organized_events')
+
+    def __str__(self):
+        return f"{self.title} by {self.organizer.username}"
+
+    @property
+    def is_upcoming(self):
+        return self.start_time > timezone.now()
+
+    @property
+    def is_past(self):
+        return self.end_time < timezone.now()
+
+    @classmethod
+    def upcoming_events(cls, user):
+        """Returns a queryset of upcoming events organized by users followed by the requesting user.
+           Assumes 'follows' field is managed elsewhere in the user model."""
+        followed_users = user.followed_by.all()
+        return cls.objects.filter(organizer__in=followed_users, start_time__gt=timezone.now()).order_by('start_time')
