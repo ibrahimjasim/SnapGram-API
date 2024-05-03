0bab736fad6b785eb39de9e1513b332e6e64b8ac
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-05-03 07:35:46 +0000

    delete the contacts model for som errors appeared

--- contacts/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Contact
-
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = ['id', 'owner', 'name', 'email', 'phone', 'address', 'created_at', 'updated_at']
--- contacts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ContactList, ContactDetail
-
-urlpatterns = [
-    path('contacts/', ContactList.as_view(), name='contact-list'),
-    path('contacts/<int:pk>/', ContactDetail.as_view(), name='contact-detail'),
-]
