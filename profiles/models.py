65645c5098054beca4edd3a598e01d335efbf5c4
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-05-07 19:51:07 +0000

    made the profiles  model compatible for pep8

--- profiles/models.py
+++ profiles/models.py
@@ -4,25 +4,36 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    """
+    Profile model to store additional information about users.
+    It has a one-to-one relationship with the User model.
+    """
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_fqfn5s'
-    )
+        upload_to='images/', default='../default_profile_fqfn5s')
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.owner}'s profile"
+        """
+        Returns a string representation of this profile,
+        featuring the username of the associated user.
+        """
+        return f"{self.owner.username}'s profile"
 
 
 def create_profile(sender, instance, created, **kwargs):
+    """
+    Signal handler that creates a Profile instance automatically
+    whenever a new User instance is created.
+    """
     if created:
         Profile.objects.create(owner=instance)
 
-post_save.connect(create_profile, sender=User)
 
+post_save.connect(create_profile, sender=User)
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -4,6 +4,11 @@ from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Profile model.
+    It includes dynamic fields that depend on the request context,
+    such as ownership status and relationship details.
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
@@ -12,16 +17,22 @@ class ProfileSerializer(serializers.ModelSerializer):
     following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
+        """
+        Returns True if the request user is the owner of the profile.
+        """
         request = self.context['request']
         return request.user == obj.owner
 
     def get_following_id(self, obj):
+        """
+        Returns the ID of a Follower
+        instance if the request user is following this profile's owner,
+        otherwise returns None.
+        """
         user = self.context['request'].user
         if user.is_authenticated:
             following = Follower.objects.filter(
-                owner=user, followed=obj.owner
-            ).first()
-            # print(following)
+                owner=user, followed=obj.owner).first()
             return following.id if following else None
         return None
 
@@ -32,3 +43,4 @@ class ProfileSerializer(serializers.ModelSerializer):
             'content', 'image', 'is_owner', 'following_id',
             'posts_count', 'followers_count', 'following_count',
         ]
+        
