d1db8b591d663c5c226f44bd44706ca6f30cb2bf
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-05-07 19:59:39 +0000

    made the comments model compatible for pep8

--- comments/views.py
+++ comments/views.py
@@ -26,3 +26,4 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
+    
